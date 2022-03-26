25ad2304d1462ef9a600e5e6a644accbe678fa76
Mayank Devrani <53875589+kakarot987@users.noreply.github.com>
2022-03-26 13:52:07 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    public static void main(String [] args){
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int test = in.nextInt();
+        List<String> arrayList = new ArrayList<String>();
+    
+            for(int cases = 1;cases<=test;cases++){
+    
+            int stringSize =in.nextInt();
+            in.nextLine();
+            while(stringSize>0){
+                String str =  in.nextLine();
+                arrayList.add(str);
+                stringSize--;
+            }
+            int result = 0;
+            for(int i=0;i<arrayList.size()-1;i++){
+                if(arrayList.get(i).compareTo(arrayList.get(i+1)) > 0){
+                    result++;
+                }
+            }
+            System.out.println("Case #" + cases + ": " + result + "\n");
+            arrayList.clear();
+        }
+        in.close();
+    }
+}
